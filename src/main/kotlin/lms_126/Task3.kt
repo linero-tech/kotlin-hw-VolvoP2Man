08d9af451043973df8b55c11156450a2a6575788
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-18 14:27:25 +0100

    Solution Task 3 Module 101

--- src/main/kotlin/lms_126/Task3.kt
+++ src/main/kotlin/lms_126/Task3.kt
@@ -4,7 +4,7 @@ fun task3(): String {
 
     return """INPUT Height of Triangle
         |INPUT Base of Triangle 
-        |CALCULATE 2X by Height times base of the Triangle
+        |CALCULATE 2X by Multiplying Height times base of the Triangle
         |CALCULATE area by dividing 2X by two
         |OUTPUT area of Triangle
     """.trimMargin()
