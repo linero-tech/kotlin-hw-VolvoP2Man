b3917368a3f64819f327b0eab215c981cc8a8b3e
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-19 16:20:03 +0100

    Solution Task 6 in Module 101

--- src/main/kotlin/lms_126/Task6.kt
+++ src/main/kotlin/lms_126/Task6.kt
@@ -2,5 +2,26 @@ package lms_126
 
 fun task6(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """VAL X = 5
+Println X*1
+println X*2
+println X*3
+println X*4
+println X’5
+Println X*6
+println X*7
+println X*8
+println X*9
+println X*10
+Println X*11
+println X*12
+println X*13
+println X*14
+println X*15
+Println X*16
+println X*17
+println X*18
+println X*19
+println X*20
+"""
 }
