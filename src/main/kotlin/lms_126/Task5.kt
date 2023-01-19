0bc25b59d9347b79a7f812358bf92ef9ec8e6b6b
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-19 16:18:10 +0100

    Solution Task 5 in Module 101

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -2,5 +2,14 @@ package lms_126
 
 fun task5(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """VAL X
+        |IF X=1 println("Monday")
+        |IF X=2 println("Tuesday")
+        |IF X=3 println("Wednesday")
+        |IF X=4 println("Thursday")
+        |IF X=5 println("Friday")
+        | IF X=6 println("Saturday")
+        | IF X=7 println("Sunday")
+        | IF X=8 println("Sunday, not a sunday")
+    """.trimMargin()
 }
