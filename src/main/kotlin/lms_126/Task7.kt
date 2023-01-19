081e2622d9c5867575b3644c450fb313c0bca332
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-19 17:18:21 +0100

    Solution Task 7 in Module 101

--- src/main/kotlin/lms_126/Task7.kt
+++ src/main/kotlin/lms_126/Task7.kt
@@ -2,5 +2,10 @@ package lms_126
 
 fun task7(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """VAR X=1
+Println X*2
+X=X+1
+Println X*2
+IF X<50 SKIP TO LINE 7
+"""
 }
