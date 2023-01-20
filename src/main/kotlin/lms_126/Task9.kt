f8a8a7a179bb1a08ed0e6310305cdd7a34b542d7
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-20 15:39:34 +0100

    Solution Task 9 Module 101

--- src/main/kotlin/lms_126/Task9.kt
+++ src/main/kotlin/lms_126/Task9.kt
@@ -2,5 +2,36 @@ package lms_126
 
 fun task9(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """VAR X 
+        VAR Balance
+        VAL SC=000000
+        Println("PIN Required to Continue, Please enter your PIN")
+        INPUT SC1
+        IF SC1=SC SKIP TO LINE 19
+         Println("Access denied, Wrong PIN, Try Again")
+         INPUT SC2 
+         IF SC2=SC SKIP TO LINE 19
+         Println("Access denied, Wrong PIN, You have one more try,Try Again")
+         INPUT SC3
+         IF SC3=SC SkIP TO LINE 19
+         Println("Access denied, Wrong PIN")
+         SKIP TO LINE 8
+         CLEAR SCREEN
+         BALANCE=500
+         Println("Your Balance is")
+         Println BALANCE
+         Println("How much would you like to withdraw from your account?")
+         INPUT X
+         CLEAR SCREEN
+         Println("withdrawing")
+         Println X
+         BALANCE-X=BALANCE
+         Println("Your current balance is now")
+         Println BALANCE
+         Println("Would you like to make another withdraw?")
+         INPUT N
+       IF N=1 SKIP TO LINE 21
+       Println("Thank you, have a nice day")
+    """.trimMargin()
+
 }
