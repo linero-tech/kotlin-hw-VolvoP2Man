136304f384c62ce3c05495732ad33f8f8662c9cb
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-24 14:59:27 +0100

    Solution Task 2 in Module 102

--- src/main/kotlin/lms_127/Task2.kt
+++ src/main/kotlin/lms_127/Task2.kt
@@ -1,6 +1,18 @@
 package lms_127
 
 fun task2() {
+    val name = "Rudolf Diesel"
+    val country = "France"
+    val month = "Mars"
+    println("Hello there $name and you are from $country born in $month")
+}
 
-    TODO("Erase this line and write your solution")
+fun main() {
+    """task2"""
+    val name = "Rudolf Diesel"
+    val country = "France"
+    val month = "Mars"
+    println("Hello there $name and you are from $country born in $month")
+
+
 }
