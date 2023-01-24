1c9a8f9cb446b1445cf10798f8bb0280288499ab
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-24 14:59:00 +0100

    Solution Task 1 Module 102

--- src/main/kotlin/lms_127/Task1.kt
+++ src/main/kotlin/lms_127/Task1.kt
@@ -1,6 +1,21 @@
 package lms_127
 
 fun task1() {
+    val countryName = "Uganda"
+    val countryCity = "Kampala"
+    val countryCurrency = "Ugandan Shilling or (UGX)"
+    println("country: $countryName")
+    println("Capital City: $countryCity")
+    println("Currency: $countryCurrency")
 
-    TODO("Erase this line and write your solution")
+}
+
+fun main(){
+    task1()
+    val countryName = "Uganda"
+    val countryCity = "Kampala"
+    val countryCurrency = "Ugandan Shilling or (UGX)"
+    println("country: $countryName")
+    println("Capital City: $countryCity")
+    println("Currency: $countryCurrency")
 }
