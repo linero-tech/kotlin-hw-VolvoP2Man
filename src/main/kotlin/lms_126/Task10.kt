f670847a52a60b1c302ed6345dbe6f2174431dc1
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-24 14:00:49 +0100

    Sulotion Task 10 Module 101

--- src/main/kotlin/lms_126/Task10.kt
+++ src/main/kotlin/lms_126/Task10.kt
@@ -2,5 +2,18 @@ package lms_126
 
 fun task10(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """VAR X
+        |VAL Password
+        |Input email address
+        |set.Min(int)=6
+        |set.MaxLength(int)=24
+        Println("Please enter password")
+        |Input Password
+        |Println("Pleas confirm your password")
+        Input X
+        |IF X=Password GoTo (17)
+        |Println("your confirmation password didn't match the one you first entered, please re-enter it, without any changes from the original")
+        |GoTo (13)
+        |Println("Your account set up") 
+    """.trimMargin()
 }
