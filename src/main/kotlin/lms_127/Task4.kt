f07a85694e294bd8159ba8a54f5fd5f0d191d85a
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-25 15:36:48 +0100

    Solution Task 4 Module 102

--- src/main/kotlin/lms_127/Task4.kt
+++ src/main/kotlin/lms_127/Task4.kt
@@ -2,5 +2,12 @@ package lms_127
 
 fun task4(base: Double, height: Double): Double {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    return
+    val base = 5.0
+    val height = 10.0
+    var result = base * height / 2
+
+    println("$result")
+
+
 }
