f0b394c0f0a05c33bcf5886454ae5694928ede06
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-25 15:52:29 +0100

    Solution Task 6 in Module 102

--- src/main/kotlin/lms_127/Task6.kt
+++ src/main/kotlin/lms_127/Task6.kt
@@ -2,5 +2,10 @@ package lms_127
 
 fun task6(base1: Double, base2: Double, height: Double): Double {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    return
+    val base1 = 10.0
+    val base2 = 20.0
+    val height = 1.0
+    val result = (base1 + base2) * height / 2
+    println("The area of a trapezoid with the base 1 of $base1 , base 2 of $base2 and height of $height is $result")
 }
