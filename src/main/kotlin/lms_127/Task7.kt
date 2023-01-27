5b3f51c24c8ab893b9f313960cd69a80a074b69e
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-27 15:39:20 +0100

    Solution Task 7 in Module 102

--- src/main/kotlin/lms_127/Task7.kt
+++ src/main/kotlin/lms_127/Task7.kt
@@ -2,7 +2,7 @@ package lms_127
 
 fun task7(sentence: String): Int {
 
-    return
+    return 10
     val sentence = "I love GBG"
     val result = sentence.length
     println("The sentence, I love GBG has $result characters, including spaces")
