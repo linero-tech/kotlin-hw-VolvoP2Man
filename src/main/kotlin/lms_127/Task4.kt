eeda5744ad89a95a76b48700182631f5e6197c13
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-30 11:10:27 +0100

    Solution Task 4 in Module 102

--- src/main/kotlin/lms_127/Task4.kt
+++ src/main/kotlin/lms_127/Task4.kt
@@ -2,12 +2,15 @@ package lms_127
 
 fun task4(base: Double, height: Double): Double {
 
-    return 25.0
     val base = 5.0
     val height = 10.0
     var result = base * height / 2
-
     println("$result")
-
-
+    return result
+}
+fun main () {
+    val base = 5.0
+    val height = 10.0
+    var result = base * height / 2
+    println("$result")
 }
