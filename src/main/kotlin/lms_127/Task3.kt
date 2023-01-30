102af2eefaf34e695aadbffbeb84236342597006
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-30 11:10:06 +0100

    Solution Task 3 in Module 102

--- src/main/kotlin/lms_127/Task3.kt
+++ src/main/kotlin/lms_127/Task3.kt
@@ -2,11 +2,16 @@ package lms_127
 
 fun task3(radius: Double): Double {
 
-    return 12.57
-
     var radius=2.0
     val pi = 3.1416
     val result = pi * radius * radius
     println("The perimeter of a circle with the radius of $radius is $result")
+    return result
+}
 
+fun main () {
+    var radius=2.0
+    val pi = 3.1416
+    val result = pi * radius * radius
+    println("The perimeter of a circle with the radius of $radius is $result")
 }
