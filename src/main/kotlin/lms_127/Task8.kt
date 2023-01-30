3a31587c7e44b953def100fda0dffdbb3815f351
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-01-30 11:03:36 +0100

    Solution Task 8 in Module 102

--- src/main/kotlin/lms_127/Task8.kt
+++ src/main/kotlin/lms_127/Task8.kt
@@ -2,7 +2,13 @@ package lms_127
 
 fun task8(sentence: String, character: Char): Int {
 
-    return 2
+    val sentence = "I code in KOTLIN"
+    val result = sentence.count{it == 'I'}
+    println ("the letter I occurs $result times in the sentence I code in Kotlin")
+    return result
+}
+
+fun main () {
     val sentence = "I code in KOTLIN"
     val result = sentence.count{it == 'I'}
     println ("the letter I occurs $result times in the sentence I code in Kotlin")
