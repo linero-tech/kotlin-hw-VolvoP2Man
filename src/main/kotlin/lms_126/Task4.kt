301de53609444466389997d90f2c8f477532397d
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-02-07 16:58:51 +0100

    Task 4 in Module 101

--- src/main/kotlin/lms_126/Task4.kt
+++ src/main/kotlin/lms_126/Task4.kt
@@ -4,9 +4,11 @@ fun task4(): String {
 
     return """VAL X
         |VAL Y 
-        IF X>Y SKIP to LINE 10
-        |println("Y has greater value than X")
-        |SKIP to Line 11
-        println("X has greater Value than Y) 
+        IF X>Y 
+        println("X has greater Value than Y)
+        else 
+        println("Y has greater value than X")
+       
+        
     """.trimMargin()
 }
