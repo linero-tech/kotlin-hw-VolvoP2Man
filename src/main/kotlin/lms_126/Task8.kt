37572f1b281573c5ebc5f7a0abda0af4c780b19b
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-02-07 18:08:13 +0100

    Solution Task 8 in Module 101

--- src/main/kotlin/lms_126/Task8.kt
+++ src/main/kotlin/lms_126/Task8.kt
@@ -2,7 +2,9 @@ package lms_126
 
 fun task8(): String {
 
-    return """VAR X
+    return
+    /* (Original Messy solution)
+VAR X
 INPUT X
 VAL Y=3
 VAL Z=5
@@ -25,6 +27,25 @@ SKIP LINE TO 21
 IF G=1 Both V=1 Println(“FooBar”) SKIP TO LINE 29
 IF V = 1 Than Println(“Foo”)
 IF G = 1 Than Println(“Bar”) 
-Println("Qix")
-"""
+Println("Qix") */
+
+
+    """ val x = ""
+        Input X
+        If X %15 == 0 
+        println("FooBar")
+        else if X %3 == =
+        println("Foo")
+        else if X %5 == 0
+        println("Bar")
+        else if X %3 != 0 || X %5 != 0 || X %15 != 0
+        println("Qix")
+        else 
+        print("Error")
+      
+       
+        
+    
+       
+    """.trimMargin()
 }
