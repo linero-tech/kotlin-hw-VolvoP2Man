8283c6112bbd712326be2ba000ae1c4225a089a7
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-02-16 19:11:08 +0100

    Solution Task 7 in Module 102

--- src/main/kotlin/lms_127/Task7.kt
+++ src/main/kotlin/lms_127/Task7.kt
@@ -2,14 +2,11 @@ package lms_127
 
 fun task7(sentence: String): Int {
 
-    val sentence = "I love GBG"
     val result = sentence.length
-    println("The sentence, I love GBG has $result characters, including spaces")
+    println("The sentence, $sentence has $result characters, including spaces")
     return result
 }
 
 fun main () {
-    val sentence = "I love GBG"
-    val result = sentence.length
-    println("The sentence, I love GBG has $result characters, including spaces")
+task7(sentence = "The Volvo B5244T5 is 5 cylinders, 2,4 liters of pure perfection")
 }
