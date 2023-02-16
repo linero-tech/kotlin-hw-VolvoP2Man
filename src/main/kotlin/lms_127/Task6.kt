129f3b852b8e15e0f924523fe7beb98ea5925a28
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-02-16 19:03:57 +0100

    Solution Task 6 in Module 102

--- src/main/kotlin/lms_127/Task6.kt
+++ src/main/kotlin/lms_127/Task6.kt
@@ -2,18 +2,12 @@ package lms_127
 
 fun task6(base1: Double, base2: Double, height: Double): Double {
 
-    val base1 = 10.0
-    val base2 = 20.0
-    val height = 1.0
+
     val result = (base1 + base2) * height / 2
     println("The area of a trapezoid with the base 1 of $base1 , base 2 of $base2 and height of $height is $result")
     return result
 }
 
 fun main () {
-    val base1 = 10.0
-    val base2 = 20.0
-    val height = 1.0
-    val result = (base1 + base2) * height / 2
-    println("The area of a trapezoid with the base 1 of $base1 , base 2 of $base2 and height of $height is $result")
+task6( base1 = 10.0 ,base2 = 20.0, height = 96.0)
 }
