5a165cb6fa254eb508a9ca8ccd2be99fc85a9938
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-02-20 17:46:59 +0100

    Solution Task 9 in Module 102

--- src/main/kotlin/lms_127/Task9.kt
+++ src/main/kotlin/lms_127/Task9.kt
@@ -2,14 +2,12 @@ package lms_127
 
 fun task9(sentence: String, character: Char): Boolean {
 
-    val sentence = "I code In KOTLIN"
-    val result = sentence.contains('i', ignoreCase = true)
+
+    val result = sentence.contains(character, ignoreCase = true)
     println("result is $result ")
     return result
 }
 
 fun main () {
-    val sentence = "I code In KOTLIN"
-    val result = sentence.contains('i', ignoreCase = true)
-    println("result is $result ")
-}
+ task9(sentence = "fuck this", character = 'i')
+    }
