da5e3189f8ed259421d315cdafcb2dc08fb29fdd
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-02-21 17:27:41 +0100

    Solution Task 8 in Module 102

--- src/main/kotlin/lms_127/Task8.kt
+++ src/main/kotlin/lms_127/Task8.kt
@@ -4,12 +4,11 @@ fun task8(sentence: String, character: Char): Int {
 
 
     val result = sentence.count{it == character}
-    println ("the letter I occurs $result times in the sentence I code in Kotlin")
     return result
 }
 
 fun main () {
-    task8(sentence = "Big, no huge, Volvo-BM diesel tractors", character = 'i' )
+    println(task8(sentence = "Big, no huge, Volvo-BM diesel tractors", character = 'i' ))
 
 
 }
