1ea34b0699168e4c87ce7b764dc5180c0789aa5c
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-02-28 18:34:11 +0100

    Solution Task 2 in Module 103

--- src/main/kotlin/lms_129/Task2.kt
+++ src/main/kotlin/lms_129/Task2.kt
@@ -1,6 +1,86 @@
 package lms_129
 
+import java.util.*
+import javax.print.attribute.standard.PrinterIsAcceptingJobs
+
 fun task2(number: Int): Boolean {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    """val number = input
+        |if number can only be divided by it self then it's a prime number 
+        |
+        |   print("Enter a number")
+    val scanner = Scanner(System.'in')
+    val number =scanner.nextLine()
+    var numberClone = number
+    var primeNumber = false
+    var divideNumber = 2
+    if (number == 2) {
+        println("number is not a prime number")
+    } else { 
+        for (numberClone  number)
+        Loop factorNumber until it is equal to Number
+        |
+        |If (number == factorNumber) then primeNumber = True 
+        |
+        |else if (factorNumber > number) then primeNumber = true
+        |
+        |else if (Number factored with factorNumber != 0) then CALCULATE factorNumber + 1
+        |
+        |else println("Shise man, an error has occured Lord Pog of Dankness, you went out of bounds my fine role of duck tape, Biltema Korv!")
+     
+     If (factorNumber > number) then primeNumber = false
+     |   else println("almost done")
+     |  
+     | if (primeNumber = True) than println("number is a prime number")
+     | else println("number is not a prime number")
+    """.trimMargin()
+    var numberClone = number
+    var primeNumber = false
+    var factorNumber = 2
+    var varefier = number * 98
+    var d39 = false
+
+    while (d39 == false) {
+        if (factorNumber == number) {
+            primeNumber = true
+            d39 = true
+
+        } else if (factorNumber > number) {
+            d39 = true
+
+        } else if (number % factorNumber != 0) {
+            factorNumber + 1
+
+        } else {
+            print("Error here we have, My fine Lord Pog of Dankness, went out of bounds you did")
+        }
+            if (factorNumber > number)
+
+
+            if (factorNumber > number) {
+            primeNumber = false
+            d39 = true
+        } else {
+            println("Almost DONE")
+        }
+
+        if (primeNumber == true) {
+            println("$number is a prime number")
+        } else {
+            println("$number is not a prime number")
+        }
+    d39 = true
+
+    }
+
+
+        val result = primeNumber
+
+
+        return result
+
 }
+  fun main() {
+     task2(number = 99)
+
+  }
