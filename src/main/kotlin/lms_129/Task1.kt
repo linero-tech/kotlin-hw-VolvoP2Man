981cf950d7f1c2409fd2eda1920164412ed3b010
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-02-28 11:42:06 +0100

    Solution Task 1 in Module 103

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -2,5 +2,25 @@ package lms_129
 
 fun task1(a: Int, b: Int): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val a2 = a.toString()
+    val b2 = b.toString()
+    var result = 0
+    var sudoResult = ""
+        if (a >= b) {
+            result = 0
+        } else if (a < b) {
+            sudoResult = a2 + b2
+            result = sudoResult.toInt()
+        } else {
+            print("Error")
+        }
+    println("Your result is $result")
+
+
+    return result
+}
+
+fun main () {
+    task1( a = 4 , b = 99 )
+
 }
