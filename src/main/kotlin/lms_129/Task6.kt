6d869d7714acdea07e34f5ccc4a217fd2f826524
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-03-02 17:14:33 +0100

    Solution Task 6 in Module 103 WIP

--- src/main/kotlin/lms_129/Task6.kt
+++ src/main/kotlin/lms_129/Task6.kt
@@ -2,5 +2,36 @@ package lms_129
 
 fun task6(number: Int): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+    val bummer = 0
+    val sentence = number.toString()
+    val length = sentence.length
+    val loop = false
+    var counter = 0
+    var reverse = sentence.reversed()
+    println("control = $reverse")
+    var d1 = sentence[1]
+    var d2 = sentence[2]
+    var d3 = sentence[3]
+    var d4 = sentence[4]
+    var d5 = sentence[5]
+    var d6 = sentence[6]
+var d88 = 'C'
+
+    while (!loop) {
+        counter += 1
+        if (counter > length) {
+            break
+        }else{
+            //d88 = sentence.first(strin)
+            println("Tomas the Dank engine")
+        }
+    }
+
+
+    return result
+}
+
+fun main () {
+    println(task6(number = 8444))
 }
