804a0c42bf5d989cd220c848068e503c89968d12
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-03-02 16:32:36 +0100

    Solution Task 5 in Module 103

--- src/main/kotlin/lms_129/Task5.kt
+++ src/main/kotlin/lms_129/Task5.kt
@@ -2,5 +2,22 @@ package lms_129
 
 fun task5(sentence: String): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val sierraAlphaAlphaBravo = false
+    var result = 0
+    //the easy way
+    val control = sentence.length
+    //println("control $control")
+
+  //Abusing the easy way to technically pass
+    while (!sierraAlphaAlphaBravo) {
+        result += 1
+        if (result == control) {
+            break
+        }
+    }
+    return result
+}
+
+fun main () {
+    println(task5(sentence = "The Volvo B5244T4 is a master piece"))
 }
