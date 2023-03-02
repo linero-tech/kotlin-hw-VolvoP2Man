0b0181ae604d6980fe6e65900e4d324c0b458119
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-03-02 16:14:26 +0100

    Solution Task 4 in Module 103

--- src/main/kotlin/lms_129/Task4.kt
+++ src/main/kotlin/lms_129/Task4.kt
@@ -2,5 +2,34 @@ package lms_129
 
 fun task4(): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+    var number = 10
+    val factor = 5
+    var oddBall = 0
+    var stretch = 0
+    //it was at this point where I got frustrated to the point of considering making a hole in the wall, sorry for the bad language.
+    var rubberDuck = 0
+
+
+   for (oddBall in number downTo 1) {
+       //println(oddBall)
+       if (oddBall % factor == 0)
+           rubberDuck += factor
+       stretch += rubberDuck
+       //println(oddBall)
+   }
+
+    result += stretch / factor
+ /*   number + 1
+for (i in 0 until number ) {
+    if (number2 % factor == 0) {
+        println()
+    }
+
+}*/
+    return result
+}
+
+fun main (){
+    print(task4())
 }
