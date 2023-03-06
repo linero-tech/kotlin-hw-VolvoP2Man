bc32a72ceea8fdb429529dd2408c6f58459ca4ee
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-03-06 14:28:56 +0100

    Solution Task 9 in Module 103

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -2,5 +2,28 @@ package lms_129
 
 fun task9(temperature: String): String {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = ""
+    val loop = false
+    var fahrenheit = false
+    var Celsius = false
+    var c = 0
+    var f = 0
+    val number = temperature
+
+    while(!loop){
+        if (temperature.contains('C',ignoreCase = true)) {
+
+
+        } else if (temperature.contains('F', ignoreCase = true)) {
+
+        }else{
+
+        }
+    }
+
+    return result
+}
+
+fun main(){
+    println(task9(temperature = "69C"))
 }
