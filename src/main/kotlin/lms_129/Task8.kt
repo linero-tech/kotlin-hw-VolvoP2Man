03dc77d7f49d2a1faa5c2bb54e509b2954f8d01a
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-03-06 13:58:15 +0100

    Solution Task 8 in Module 103

--- src/main/kotlin/lms_129/Task8.kt
+++ src/main/kotlin/lms_129/Task8.kt
@@ -2,5 +2,32 @@ package lms_129
 
 fun task8(number: Int): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val loop = false
+    val numberClone = number
+    val string = numberClone.toString()
+    var charTS = '0'
+    var stringToInt = ""
+    var result = 0
+    var get = 0
+    val length = string.length
+    var counter = 0
+
+    while (!loop) {
+        charTS = string[counter]
+        stringToInt = charTS.toString()
+        get += stringToInt.toInt()
+        counter += 1
+
+        if (counter == length) {
+            break
+        }
+    }
+
+    result += get
+
+    return result
+}
+
+fun main(){
+    println(task8(number = 123))
 }
