ed7d1cd4f587b6fc416f7c179ee5f73b651f9223
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-03-06 10:27:43 +0100

    Solution Task 6 in Module 103

--- src/main/kotlin/lms_129/Task6.kt
+++ src/main/kotlin/lms_129/Task6.kt
@@ -3,35 +3,31 @@ package lms_129
 fun task6(number: Int): Int {
 
     var result = 0
-    val bummer = 0
     val sentence = number.toString()
     val length = sentence.length
     val loop = false
     var counter = 0
-    var reverse = sentence.reversed()
-    println("control = $reverse")
-    var d1 = sentence[1]
-    var d2 = sentence[2]
-    var d3 = sentence[3]
-    var d4 = sentence[4]
-    var d5 = sentence[5]
-    var d6 = sentence[6]
-var d88 = 'C'
+    val reverse = sentence.reversed()
+   // println("control = $reverse")
 
+    //Useless loop
     while (!loop) {
         counter += 1
         if (counter > length) {
             break
         }else{
-            //d88 = sentence.first(strin)
-            println("Test")
+            //d88 = sentence.first(string)
+           // println("Test")
         }
     }
 
+//println(counter)
+    val result2 = reverse.toInt()
+result += result2
 
     return result
 }
 
 fun main () {
-    println(task6(number = 8444))
+    println(task6(number = 12345))
 }
