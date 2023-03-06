2ca28ae322cb09735c36b5dc917696ffdc064baa
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-03-06 11:42:47 +0100

    Solution Task 7 in Module 103 WIP

--- src/main/kotlin/lms_129/Task7.kt
+++ src/main/kotlin/lms_129/Task7.kt
@@ -2,5 +2,31 @@ package lms_129
 
 fun task7(a: Int, b: Int): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var loop = false
+    var result = 0
+    var counter = 0
+    var i = b
+
+    while(!loop) {
+        if (a == 0 && b == 0) {
+            result = 1
+            break
+        }
+
+        counter += 1
+        i += i * b
+        if (counter == a) {
+            break
+        }
+
+    }
+    i /= b
+    i /= 2
+result = i
+    return result
+}
+
+
+fun main(){
+   println(task7(a = 2, b = 3))
 }
