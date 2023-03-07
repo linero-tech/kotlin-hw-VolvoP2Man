1a962d2ad64a7d41351b998bf3537485e64db2c4
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-03-07 14:59:45 +0100

    Solution Task 10 in Module 103

--- src/main/kotlin/lms_129/Task10.kt
+++ src/main/kotlin/lms_129/Task10.kt
@@ -2,5 +2,33 @@ package lms_129
 
 fun task10(password: String): Boolean {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = false
+    val length = password.length
+    var isLongButNoTo = false
+    var character = false
+
+    if (length <= 10 && length >= 6) {
+        isLongButNoTo = true
+    }else{
+        println("Password is $length long")
+        println("Password is ether to long or to short, it has to be between 6 and 10 characters")
+    }
+
+    if (password.contains('$') || password.contains('#') || password.contains('@')) {
+        character = true
+    }else{
+        println("password $password , doesn't contain '#' or '$' or '@'")
+    }
+
+    if (isLongButNoTo == true && character == true){
+        result = true
+    }
+
+
+
+    return result
+}
+
+fun main(){
+    println(task10(password = "#clocksonS"))
 }
