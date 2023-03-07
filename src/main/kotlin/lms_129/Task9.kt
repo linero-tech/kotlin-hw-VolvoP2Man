28cc9e26de2c9767dcd39a1515bcfae073fe2823
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-03-07 14:17:26 +0100

    Solution Task 9 in Module 103

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -3,32 +3,38 @@ package lms_129
 fun task9(temperature: String): String {
 
     var result = ""
-    val loop = false
     var numberString = temperature
-    var temperatureClone = temperature.uppercase()
+    val temperatureClone = temperature.uppercase()
     var number = 0
+    var toInt = 0.0
+    var toString = 0
 
-    while(!loop){
         if (temperature.contains('C',ignoreCase = true)) {
             numberString = temperatureClone.substringBeforeLast('C')
             number = numberString.toInt()
-            result += number * 1.8 + 32
-            break
+            toInt += number * 1.8 + 32
+            toString += toInt.toInt()
+            result += toString.toString() + "F"
+
+
 
 
 
         } else if (temperature.contains('F', ignoreCase = true)) {
             numberString = temperatureClone.substringBeforeLast('F')
             number = numberString.toInt()
-            result += (number -32) * 0.5556
-            break
+            toInt += (number -32) * 0.5556
+            toString += toInt.toInt()
+            result += toString.toString() + "F"
+
+
 
         }else{
             println("The input lacks acceptable unit, please re-enter the temperature with ether 'C' for Celsius or 'F' for Fahrenheit, at the end")
-            break
+
 
         }
-    }
+
 
     return result
 }
