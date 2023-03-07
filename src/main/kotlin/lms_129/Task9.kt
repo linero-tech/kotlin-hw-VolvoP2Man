4197402ce6294c41658182e9b0bc4866485d3fc8
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-03-07 11:49:47 +0100

    Solution Task 9 in Module 103

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -8,13 +8,20 @@ fun task9(temperature: String): String {
     var Celsius = false
     var c = 0
     var f = 0
-    val number = temperature
+    var numberString = temperature
+    var number = 0
+    val fahrenheitTemp = ""
 
     while(!loop){
         if (temperature.contains('C',ignoreCase = true)) {
+            numberString = temperature.substringBeforeLast('C')
+            number = numberString.toInt()
+
 
 
         } else if (temperature.contains('F', ignoreCase = true)) {
+            numberString = temperature.substringBeforeLast('F')
+            number = numberString.toInt()
 
         }else{
 
