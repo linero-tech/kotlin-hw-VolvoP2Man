8a67d0eeb415475b375632a3a516e85fe9af9f65
VolvoP2Man <d6.t5.d4.t3.d2@gmail.com>
2023-03-09 14:20:00 +0100

    Solution Task 8 in Module 103

--- src/main/kotlin/lms_129/Task8.kt
+++ src/main/kotlin/lms_129/Task8.kt
@@ -2,28 +2,10 @@ package lms_129
 
 fun task8(number: Int): Int {
 
-    val loop = false
-    val numberClone = number
-    val string = numberClone.toString()
-    var charTS = '0'
-    var stringToInt = ""
     var result = 0
-    var get = 0
-    val length = string.length
-    var counter = 0
-
-    while (!loop) {
-        charTS = string[counter]
-        stringToInt = charTS.toString()
-        get += stringToInt.toInt()
-        counter += 1
-
-        if (counter == length) {
-            break
-        }
+    number.toString().forEach {digit ->
+        result += digit.digitToInt()
     }
-
-    result += get
 
     return result
 }
